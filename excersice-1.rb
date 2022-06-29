1aa5de3f95526079d2682d84dfca12617844c664
Vladimir <vladimiremiliojc@outlook.com>
2022-06-29 16:45:47 -0400

    Exercise 1: A Good First Program

---
+++ excersice-1.rb
@@ -0,0 +1,7 @@
+puts "Hello World!"
+puts "Hello People"
+puts "I like Pizza"
+puts "This is amazing"
+puts "Yes! ruby forever!!!"
+puts "I dont know what to write in this line"
+puts 'Is time to "finish"'
