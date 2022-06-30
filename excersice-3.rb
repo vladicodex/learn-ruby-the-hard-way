08e5efc83118001dac40406a104812b331de62d7
Vladimir <vladimiremiliojc@outlook.com>
2022-06-29 23:41:21 -0400

    Exercise 3: Numbers and Math

---
+++ excersice-3.rb
@@ -0,0 +1,19 @@
+puts "NUMBERS AND OPERATORS:"
+
+puts "#{20 + 20 / 10}"
+puts "#{50 - 25 * 2 % 5}"
+
+puts "COntinue operating:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
