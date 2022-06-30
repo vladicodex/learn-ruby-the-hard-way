114670b689365c056154232f82e156490ccb147d
Vladimir <vladimiremiliojc@outlook.com>
2022-06-30 16:51:51 -0400

    Exercise 5: More Variables and Printing

---
+++ excersice-5.rb
@@ -0,0 +1,17 @@
+my_name = 'Vladimir J'
+my_age = 21
+my_height = 166 # CM
+my_weight = 230 # LBS
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually that's so heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
