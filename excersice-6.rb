c663f207ebc32e3a8c2b118e2ed61556fd03f240
Vladimir <vladimiremiliojc@outlook.com>
2022-06-30 16:57:11 -0400

    Exercise 6: Strings and Text

---
+++ excersice-6.rb
@@ -0,0 +1,21 @@
+types_of_people = 2
+x = "There are #{types_of_people} types of people."
+man = "man"
+woman= "woman"
+y = "Those who are #{man} and those who are #{woman}."
+
+puts x
+puts y
+
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+w = "This is the left side of... "
+e = "a string with a right side."
+
+puts w + e
