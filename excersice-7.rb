0f42246ad4fc47915eb6f58675c1a3308da55c7c
Vladimir <vladimiremiliojc@outlook.com>
2022-06-30 17:11:37 -0400

    Exercise 7: More Printing

---
+++ excersice-7.rb
@@ -0,0 +1,20 @@
+puts "Vladi had a little pet."
+puts "Its face was white as #{'snow'}." # IDK
+puts "And everywhere that Vladi went."
+puts "." * 5 # OH WAO
+
+end1 = "H"
+end2 = "E"
+end3 = "L"
+end4 = "L"
+end5 = "O"
+end6 = " "
+end7 = "W"
+end8 = "O"
+end9 = "R"
+end10 = "L"
+end11 = "D"
+
+# Result of PRINT vs PUTS?
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11
