fcdf9593985110fb9ce0970510f443263dcf1b57
Vladimir <vladimiremiliojc@outlook.com>
2022-06-29 23:37:05 -0400

    Exercise 2: Comments and Pound Characters

---
+++ excersice-2.rb
@@ -0,0 +1,8 @@
+# This is a comment.
+# This another comment.
+
+puts "OMG I AM COMMENTING" # And this comment is been ignored
+
+# puts "IS THE SAME RUBY & RUBY ON RAILS ?????????????????"
+
+puts "I DONT KNOW"
