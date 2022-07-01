788f4383d88cb80d54b3a3a0f004b6a33e09d0e6
Vladimir <vladimiremiliojc@outlook.com>
2022-06-30 20:09:02 -0400

    Exercise 10: What Was That?

---
+++ excersice-10.rb
@@ -0,0 +1,15 @@
+tabby_dog = "\tI'm tabbed in."
+persian_dog = "I'm split\non a line."
+backslash_dog = "I'm \\ a \\ dog."
+
+fat_dog = """
+I'll do a list:
+\t* dog food
+\t* Fishies
+\t* dognip\n\t* Grass
+"""
+
+puts tabby_dog
+puts persian_dog
+puts backslash_dog
+puts fat_dog
