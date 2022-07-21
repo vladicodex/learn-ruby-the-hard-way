0dce552d5d25d88b68046f864644d6599c15fe0d
Vladimir <vladimiremiliojc@outlook.com>
2022-07-20 23:00:50 -0400

    Exercise 23: Read Some Code

---
+++ excersice-23.rb
@@ -0,0 +1,5 @@
+#1. Go to github.com with your favorite web browser and search for "ruby."
+#2. Pick a random project and click on it.
+#3. Click on the Source tab and browse through the list of files and directories until you find a .rb file.
+#4. Start at the top and read through the code, taking notes on what you think it does.
+#5. If any symbols or strange words seem to interest you, write them down to research later.
