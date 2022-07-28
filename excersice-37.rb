7a205343b99cb56a0253472a7db841ce5fb3295f
Vladimir <vladimiremiliojc@outlook.com>
2022-07-27 23:20:54 -0400

    Exercise 37: Symbol Review

---
+++ excersice-37.rb
@@ -0,0 +1,2 @@
+# It's time to review the symbols and Ruby words you know and to try to pick up a few more for the next few lessons.
+# I have written out all the Ruby symbols and keywords that are important to know.
